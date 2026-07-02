d455ec00666707962ea2595714f38ac1109de293
pagarkristian <akunkerja23128@gmail.com>
2026-07-03 00:35:01 +0700

    feat: display HTTP response headers

--- src/main.py
+++ src/main.py
@@ -6,8 +6,23 @@ def display_banner():
     """
 
     print("=" * 50)
-    print("HTTP HEADER SCANNER V1.0")
+    print("HTTP HEADER SCANNER V1.1")
     print("=" * 50)
+
+def display_headers(headers):
+    """
+    Display all HTTP response headers.
+
+    Args:
+        headers (dict): HTTP response headers.
+    """
+
+    print("\nResponse Headers")
+    print("-" * 50 )
+
+    for key, value in headers.items():
+        print(f"{key:<30}: {value}")
+
 
 def main():
     """
@@ -24,6 +39,9 @@ def main():
     print("\n Request completed successfully. \n")
     print(f"Final Url: {response.url}")
     print(f"Status Code : {response.status_code}")
+
+    display_headers(response.headers)
+
 
 if __name__== "__main__":
     main()
